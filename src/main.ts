6fd85d0fc51d35385dcdaf9cce443cb2d4d5bafe
Рауль <raul@MacBook-Air-Raul.local>
2026-02-12 23:18:12 +0300

    backend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(process.cwd(), 'public'));
+  app.setBaseViewsDir(join(process.cwd(), 'views'));
+  app.setViewEngine('hbs');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
